b4be32dfc03e0b019e9bfc5431bcf1c53f27a869
Sanky <gsanky@gmail.com>
2014-05-13 02:21:47 +0200

    relative_search.c: cleanup

--- relative_search.c
+++ relative_search.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
+int i;
+int matched;
+unsigned char off;
+char c;
+char* string;
+int pos = 0;
+FILE *fp;
+
+int match_string(int is16bit) {
+    off = string[0] - c;
+    
+    matched = 1;
+    for (i=1; i<strlen(string); i++) {
+        if (is16bit) fgetc(fp);
+        if ((unsigned char)(string[i] - fgetc(fp)) != off) break;
+        matched++;
+    }
+    if (matched == strlen(string)) {
+        printf(" %d-bit string found at pos 0x%x, offset by 0x%x\n", is16bit ? 16 : 8, pos, off);
+        i--;
+    }
+    
+    
+    if (feof(fp)) return -1;
+    fseek(fp, -i * (is16bit ? 2 : 1), SEEK_CUR);
+}
+
 int main(int argc, char *argv[]) {
     int argi;
-    int i;
-    int pos = 0;
-    int matched;
-    unsigned char off;
-    int is16bit = 0;
-    char c;
     char* filename;
+    
     if (argc < 3) {
         printf("Usage: ./relative_search string file [...]\n");
         return 1;
     }
-    char* string = argv[1];
+    string = argv[1];
     for (argi=2; argi < argc; argi++) {
         filename = argv[argi];
         printf("Looking for string %s in %s\n", string, filename);
-        FILE *fp;
         
         fp = fopen(filename, "rb");
         
@@ -30,24 +51,8 @@ int main(int argc, char *argv[]) {
         while((c = fgetc(fp)) != EOF) {
             off = string[0] - c;
             
-            do {
-                matched = 1;
-                for (i=1; i<strlen(string); i++) {
-                    if (is16bit) getc(fp);
-                    if ((unsigned char)(string[i] - getc(fp)) != off) break;
-                    matched++;
-                }
-                if (matched == strlen(string)) {
-                    printf(" %d-bit string found at pos 0x%x, offset by 0x%x\n", is16bit ? 16 : 8, pos, off);
-                    i--;
-                }
-                
-                
-                if (feof(fp)) break;
-                fseek(fp, -i * (is16bit ? 2 : 1), SEEK_CUR);
-                
-                is16bit = !is16bit;
-            } while (is16bit != 0);
+            match_string(0);
+            match_string(1);
             
             pos++;
         }
